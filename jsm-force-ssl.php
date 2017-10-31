d64b6989889c77cee7dfb6204da612815064f802
JS Morisset <jsm@surniaulula.com>
2017-10-31 08:41:47 -0600

    v1.2.0

--- jsm-force-ssl.php
+++ jsm-force-ssl.php
@@ -10,9 +10,9 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Safe, amazingly fast, simple and effective - force HTTP URLs to HTTPS using WordPress filters and permanent redirects.
+ * Requires PHP: 5.3
  * Requires At Least: 3.7
  * Tested Up To: 4.8.3
- * Requires PHP: 5.3
  * Version: 1.2.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
