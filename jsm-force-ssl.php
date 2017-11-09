6797e196043a66052134bca9c674c30e0b2accc2
JS Morisset <jsm@surniaulula.com>
2017-11-08 20:58:31 -0600

    v1.2.0

--- jsm-force-ssl.php
+++ jsm-force-ssl.php
@@ -12,7 +12,7 @@
  * Description: Safe, amazingly fast, simple and effective - force HTTP URLs to HTTPS using WordPress filters and permanent redirects.
  * Requires PHP: 5.3
  * Requires At Least: 3.7
- * Tested Up To: 4.8.3
+ * Tested Up To: 4.9
  * Version: 1.2.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
