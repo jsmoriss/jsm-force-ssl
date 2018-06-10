7368386ff82aedba138b8d02abab44061f84fae2
JS Morisset <jsm@surniaulula.com>
2018-06-10 19:52:49 -0400

    v1.2.1

--- jsm-force-ssl.php
+++ jsm-force-ssl.php
@@ -83,9 +83,11 @@ if ( ! class_exists( 'JSM_Force_SSL' ) ) {
 		}
 
 		public static function &get_instance() {
+
 			if ( ! isset( self::$instance ) ) {
 				self::$instance = new self;
 			}
+
 			return self::$instance;
 		}
 
