1f194460dc4b884729595214a1a67e508e8f6d85
jsmoriss <jsm@surniaulula.com>
2024-06-25 04:07:36 +0000

    v3.4.1

--- jsm-force-ssl.php
+++ jsm-force-ssl.php
@@ -12,7 +12,7 @@
  * Description: No setup required - simply activate to force HTTP URLs to HTTPS using native WordPress filters and permanent redirects for best SEO.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.8
- * Tested Up To: 6.5.4
+ * Tested Up To: 6.5.5
  * Version: 3.4.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
