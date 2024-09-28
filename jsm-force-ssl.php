b3521fada904367acb3b907b9af92fa2e62103ce
jsmoriss <jsm@surniaulula.com>
2024-09-28 17:32:55 +0000

    v3.5.0

--- jsm-force-ssl.php
+++ jsm-force-ssl.php
@@ -13,7 +13,7 @@
  * Requires PHP: 7.4.33
  * Requires At Least: 5.9
  * Tested Up To: 6.6.2
- * Version: 3.4.1
+ * Version: 3.5.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
