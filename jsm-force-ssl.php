37e51c695bf23e63bb5cba552b4f048d3a7d200e
jsmoriss <jsm@surniaulula.com>
2025-12-26 16:24:44 +0000

    v3.5.0

--- jsm-force-ssl.php
+++ jsm-force-ssl.php
@@ -11,7 +11,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: No setup required - simply activate to force HTTP URLs to HTTPS using native WordPress filters and permanent redirects for best SEO.
  * Requires PHP: 7.4.33
- * Requires At Least: 5.9
+ * Requires At Least: 6.0
  * Tested Up To: 6.9
  * Version: 3.5.0
  *
